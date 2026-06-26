898e7594d03c88e837297842ae4ad860067c5ece
BenjaminAlvarenga <20240080@ricaldone.edu.sv>
2026-06-26 15:34:34 -0600

    Agregacion de cors

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,17 @@
 import express from "express";
 import productRoutes from "./src/routes/products.js";
 import branchRoutes from "./src/routes/branches.js";
-import employeeRoutes from "./src/routes/employees.js"
-import brandRoutes from "./src/routes/brands.js"
-import adminRoutes from "./src/routes/admins.js"
+import employeeRoutes from "./src/routes/employees.js";
+import brandRoutes from "./src/routes/brands.js";
+import adminRoutes from "./src/routes/admins.js";
 import customerRoutes from "./src/routes/customers.js";
-import registerCustomerRoutes from "./src/routes/registerCustomer.js"
+import registerCustomerRoutes from "./src/routes/registerCustomer.js";
 import loginCustomerRoutes from "./src/routes/loginCustomer.js";
-import logoutRoutes from "./src/routes/logout.js"
-import ProviderRoutes from "./src/routes/providers.js"
-import cartRoutes from "./src/routes/cart.js"
+import logoutRoutes from "./src/routes/logout.js";
+import ProviderRoutes from "./src/routes/providers.js";
+import cartRoutes from "./src/routes/cart.js";
 import cookieParser from "cookie-parser";
-import wompiRoutes from "./src/routes/wompi.js"
+import wompiRoutes from "./src/routes/wompi.js";
 import deliveryDriverRoutes from "./src/routes/deliveryDriver.js";
 import limiter from "./src/middlewares/rateLimiter.js";
 import { validateAuthCookie } from "./src/middlewares/authMiddleware.js";
@@ -22,23 +22,37 @@ const app = express();
 app.use(cookieParser());
 app.use(limiter);
 
+app.use(
+  cors({
+    origin: [
+      "https://pricesmart20240080.onrender.com",
+      "http://localhost:5173",
+      "http://localhost:5174",
+    ],
+
+    //Permitir el envío de cookies y credenciales
+
+    credentials: true,
+  })
+);
+
 //Aceptar JSON en postman
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 app.use("/api/branches", branchRoutes);
 app.use("/api/employees", validateAuthCookie(["admin"]), employeeRoutes);
-app.use("/api/brands", brandRoutes)
-app.use("/api/admins", adminRoutes)
-app.use("/api/customers", customerRoutes)
-app.use("/api/registerCustomers", registerCustomerRoutes)
-app.use("/api/registerAdmin", registerAdminRoutes)
-app.use("/api/loginCustomer", loginCustomerRoutes)
-app.use("/api/loginAdmin", loginAdminRoutes)
-app.use("/api/logout", logoutRoutes)
-app.use("/api/providers", ProviderRoutes)
-app.use("/api/cart", validateAuthCookie(["admin", "customer"]) ,cartRoutes)
-app.use("/api/wompi", wompiRoutes)
-app.use("/api/deliveryDrivers", deliveryDriverRoutes)
+app.use("/api/brands", brandRoutes);
+app.use("/api/admins", adminRoutes);
+app.use("/api/customers", customerRoutes);
+app.use("/api/registerCustomers", registerCustomerRoutes);
+app.use("/api/registerAdmin", registerAdminRoutes);
+app.use("/api/loginCustomer", loginCustomerRoutes);
+app.use("/api/loginAdmin", loginAdminRoutes);
+app.use("/api/logout", logoutRoutes);
+app.use("/api/providers", ProviderRoutes);
+app.use("/api/cart", validateAuthCookie(["admin", "customer"]), cartRoutes);
+app.use("/api/wompi", wompiRoutes);
+app.use("/api/deliveryDrivers", deliveryDriverRoutes);
 
 export default app;

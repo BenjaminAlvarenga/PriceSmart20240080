ca04615e7c142fd8ce82e34f576b3990e5e67371
BenjaminAlvarenga <20240080@ricaldone.edu.sv>
2026-06-26 15:39:51 -0600

    Solucion de error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/UI/Nav";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Form from "./pages/Form"
 import Customers from "./pages/Customers";
 
